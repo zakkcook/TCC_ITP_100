e94a0984a243ccb69e80c5ada5bc4787fe0f247b
Zakk Cook <zakkary.c.cook@gmail.com>
2023-01-14 23:43:12 -0500

    completed Lab2 extra credit

---
+++ Lab2-extra-credit.py
@@ -0,0 +1,52 @@
+import turtle as t
+
+
+def main():
+    try1()
+
+    input()
+
+def try1():
+    setup()
+    colors = ['blue', 'yellow', 'black', 'green', 'red', 'pink', 'brown']
+    x_pos = -300
+    y_pos = 0
+
+    for color in colors:
+        t.goto(x_pos, y_pos)
+        drawCircle(color)
+        x_pos = x_pos + 130
+        y_pos = 0 if y_pos == -100 else -100
+
+def try2():
+    setup()
+
+    t.goto(-300,0)
+    drawCircle('blue')
+
+    t.goto(-170,-100)
+    drawCircle('yellow')
+  
+    t.goto(-40,0)
+    drawCircle('black')
+
+    t.goto(90,-100)
+    drawCircle('green')
+
+    t.goto(220,0)
+    drawCircle('red')
+
+def setup():
+    t.hideturtle()
+    t.penup()
+    t.pensize(5)
+
+def drawCircle(color, size = 100):
+    t.pencolor(color)
+    t.pendown()
+    t.circle(size)
+    t.penup()
+
+
+if __name__ == "__main__":
+    main()
