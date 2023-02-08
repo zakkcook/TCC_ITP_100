3c138c4f364422cb1fe4fd3ea0ab6d83da611f75
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-08 01:16:06 -0500

    Rough draft

--- Lab4_Zakkary_Cook/Lab4c.py
+++ Lab4_Zakkary_Cook/Lab4c.py
@@ -1,3 +1,15 @@
 '''
 write a program that uses nested loops to draw this pattern:
 '''
+x = 7
+y = 7
+z = ""
+
+
+while(y>0):
+    while(x>0):
+        z = z + '*'
+        x = x - 1
+    print(z)
+    y = y - 1
+# x = 1, y = 1
