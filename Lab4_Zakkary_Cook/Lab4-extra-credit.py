cf823422371b82a21798ebe7c731affe5cd66426
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-08 13:43:44 -0500

    Lab4 Completed

--- Lab4_Zakkary_Cook/Lab4-extra-credit.py
+++ Lab4_Zakkary_Cook/Lab4-extra-credit.py
@@ -1,3 +1,18 @@
-'''
+"""
 use a loop with the turtle graphics library to draw the design shown:
-'''
+"""
+import turtle as t
+
+def main():
+    t.pensize(1)
+    t.pencolor("black")
+
+    t.pendown()
+    for i in range(50):
+        t.forward(10 * i)
+        t.left(90)
+
+    input()
+
+if __name__ == "__main__":
+    main()
--- Lab4_Zakkary_Cook/Lab4c.py
+++ Lab4_Zakkary_Cook/Lab4c.py
@@ -1,15 +1,23 @@
-'''
+"""
 write a program that uses nested loops to draw this pattern:
-'''
-x = 7
-y = 7
-z = ""
 
+*******
+******
+*****
+****
+***
+**
+*
+"""
 
-while(y>0):
-    while(x>0):
-        z = z + '*'
-        x = x - 1
-    print(z)
-    y = y - 1
-# x = 1, y = 1
+rows = 7
+result = ""
+
+for r in range(rows, 0, -1):  # Loop from 7(rows) to 1
+    result = ""  # Set result to empty string each time we start
+    for star in range(0, rows):  # Loop from 0 to 6 (e.g. 7times, based on row)
+        result = result + "*"  # Add a star to the result
+
+    print(result)  # print the result
+    result = ""  # reset result to be an empty string
+    rows = rows - 1  # Reset next stars_tto_print to be the next count
