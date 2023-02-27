8d324335b59f482174d071fc350abdd9cced28a4
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-27 18:38:58 -0500

    Lab3c cleaned up

--- Lab3_Zakkary_Cook/Lab3c.py
+++ Lab3_Zakkary_Cook/Lab3c.py
@@ -1,4 +1,5 @@
-# A software company sells a package that retails for $99. Quantity discounts are given according to the following table.
+# A software company sells a package that retails for $99. Quantity discounts
+# are given according to the following table.
 '''
 10-19 get 10%
 20-49 gets 20%
@@ -6,13 +7,15 @@
 100+ gets 40%
 '''
 
+
 def main():
     packagesPurchased = int(input("Enter the number of packages purchased: "))
     printReceipt(packagesPurchased)
-    
+
+
 def getDiscount(packagesPurchased):
     discount = .01
-    
+
     if packagesPurchased > 99:
         discount = .40
     elif packagesPurchased >= 50 and packagesPurchased <= 99:
@@ -32,10 +35,11 @@ def printReceipt(packagesPurchased):
     discountPct = getDiscount(packagesPurchased)
     discountAmount = packagesPurchased * packageCost * discountPct
     totalAmount = packagesPurchased * packageCost - discountAmount
-    
+
     print(f"Discount Given: {discountPct:,.0%}")
     print(f"Discount Amount: ${discountAmount:,.2f}")
     print(f"Total Amount: ${totalAmount:,.2f}")
+
 
 if __name__ == "__main__":
     main()
