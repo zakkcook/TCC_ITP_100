631e4c54cf03062fc2f7529fb5e5a4a3fdef2e47
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-28 14:26:40 -0500

    Midterm Lab4 completed

--- Midterm_Zakkary_Cook/Lab4.py
+++ Midterm_Zakkary_Cook/Lab4.py
@@ -7,37 +7,46 @@ program should display their sum.
 
 
 def main():
-
     total = 0.00
-    number = float(input('Enter a positive number (negative to quit): '))
-    while number >= 0:
-        number = float(input('Enter a positive number (negative to quit): '))
+    number = 0.00
+    output = []
+
+    # User inputs a list of positive numbers
+    while number >= 0.00:
         total = total + number
-    print('Total = ', total)
+        number = float(input('Enter a positive number (negative to quit): '))
+        if number == 0.00:
+            print('Zero is not a positive number. Try again!')
+        elif number < 0.00:
+            break
+        else:
+            output.append(str(f'{number:,.1f}'))
+
+    print(f'Total = {total:,.2f}')
+    myString = buildTextString(output)
+    writeToFile('data.txt', myString)
+
+
+def buildTextString(stringList):
+    mystring = ''
+    total = 0
+
+    # Loop thru list and build string
+    for x in range(len(stringList)):
+        total += float(stringList[x])
+        if x < len(stringList) - 1:
+            mystring += f'{stringList[x]} + '
+        else:
+            mystring += f'{stringList[x]} = {total}'
+
+    return mystring
+
+
+def writeToFile(file, content):
+    with open(file, 'w') as f:
+        f.write(content)
+    f.close()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-    number = ''
-    while number >= 0:
-        number = float(input("Enter a positive number (negative to quit): "))
-
-    if number >= 0:
-        return True
-    else:
-        return False
-
-'''
