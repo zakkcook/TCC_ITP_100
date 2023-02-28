497d941172e6f16c038e5ab5f4202395071f9be8
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-28 01:00:36 -0500

    Midterm Lab4 rough draft

--- Midterm_Zakkary_Cook/Lab4.py
+++ Midterm_Zakkary_Cook/Lab4.py
@@ -4,3 +4,40 @@ positive numbers. The user should enter a negative number to signal the end
 of the series. After all the positive numbers have been entered, the
 program should display their sum.
 '''
+
+
+def main():
+
+    total = 0.00
+    number = float(input('Enter a positive number (negative to quit): '))
+    while number >= 0:
+        number = float(input('Enter a positive number (negative to quit): '))
+        total = total + number
+    print('Total = ', total)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    number = ''
+    while number >= 0:
+        number = float(input("Enter a positive number (negative to quit): "))
+
+    if number >= 0:
+        return True
+    else:
+        return False
+
+'''
