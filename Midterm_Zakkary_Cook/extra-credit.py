e9939d208195de8c6ce6eb308631cd391c38c5bb
Zakk Cook <zakkary.c.cook@gmail.com>
2023-02-28 19:18:15 -0500

    Midterm extra credit completed

--- Midterm_Zakkary_Cook/extra-credit.py
+++ Midterm_Zakkary_Cook/extra-credit.py
@@ -9,3 +9,23 @@ Starting number of organisms: 2
 Average daily increase: 30%
 Number of days to multiply: 10
 '''
+
+organisms = -1
+while organisms <= 0:
+    organisms = int(input('Starting number of organisms: '))
+
+average = -1.0
+while average <= 0:
+    average = float(input('Average daily increase: '))
+    if average >= 1:
+        average = float(average) / 100
+
+nbr_days = -1
+while nbr_days <= 0:
+    nbr_days = int(input('Number of days to multiply: '))
+
+print('Day\tApproximate Population')
+print('----------------------------------')
+for d in range(1, nbr_days+1):
+    print(f'{d}\t{organisms}')
+    organisms = organisms + (organisms * average)
