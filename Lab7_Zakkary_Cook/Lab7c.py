cde292c500700720e07acbd00cfd2f77354c2a6f
Zakk Cook <zakkary.c.cook@gmail.com>
2023-03-22 12:41:56 -0400

    Lab7c completed

--- Lab7_Zakkary_Cook/Lab7c.py
+++ Lab7_Zakkary_Cook/Lab7c.py
@@ -6,10 +6,23 @@ all of the numbers in the list that are greater than the number n.
 
 
 def main():
-    pass
-    # create a list of numbers
-    # create a number N
-    #
+
+    listofnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    n = 5
+
+    new_list = print_numbers_greater_than(n, listofnumbers)
+    print('List of numbers:')
+    print(listofnumbers)
+    print(f'List of numbers that are greater than {n}:')
+    print(new_list)
+
+
+def print_numbers_greater_than(min_number, listofnumbers):
+    my_new_list = []
+    for number in listofnumbers:
+        if number > min_number:
+            my_new_list.append(number)
+    return my_new_list
 
 
 if __name__ == "__main__":
