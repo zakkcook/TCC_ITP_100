dd7889168d177621be8ca7cea4107cc848a6f737
Zakk Cook <zakkary.c.cook@gmail.com>
2023-03-22 11:55:57 -0400

    Lab7c rough draft

--- Lab7_Zakkary_Cook/Lab7c.py
+++ Lab7_Zakkary_Cook/Lab7c.py
@@ -3,3 +3,14 @@ In a program, write a function that accepts two arguments: a list, and a
 number n. Assume that the lists contains numbers. The function should display
 all of the numbers in the list that are greater than the number n.
 '''
+
+
+def main():
+    pass
+    # create a list of numbers
+    # create a number N
+    #
+
+
+if __name__ == "__main__":
+    main()
