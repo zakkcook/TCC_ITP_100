27cd87e8e90a8d6a01f9032ad7d959965597cd67
Zakk Cook <zakkary.c.cook@gmail.com>
2023-04-16 19:32:31 -0400

    Project Info

---
+++ Project_Zakkary_Cook/Project.py
@@ -0,0 +1,5 @@
+''' Students will develop a website with everything they have learned
+during the semester. The theme of the project is up to the student.
+Your code should use variables, conditions, loops, lists, dictionaries,
+objects, GUI, etc. Students will present their project to the class
+and explain and answer questions related to the code(5 - 15 minutes).'''
