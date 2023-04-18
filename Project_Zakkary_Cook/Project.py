ff756cb168b0fd0a693623efaee8ee14db925d66
Zakk Cook <zakkary.c.cook@gmail.com>
2023-04-18 17:20:25 -0400

    project rough draft

--- Project_Zakkary_Cook/Project.py
+++ Project_Zakkary_Cook/Project.py
@@ -28,6 +28,21 @@ def main():
     writeHTML(name, year)
 
 
+def getLetterFromName(name):
+    result = 'A'
+    return result
+
+
+def getMovieWithLetterInTitle(letter_from_name):
+    result = 'Quarantine.jpg'
+    return result
+
+
+def GetMovieBasedOnBirthYear(year):
+    result = 'Indiana Jones.jpg'
+    return result
+
+
 def isInvalidName(name: str) -> bool:
     '''If the name contains at least one letter, then its valid'''
     for character in name:
@@ -59,83 +74,60 @@ def isInvalidYear(year: int) -> bool:
 
 def getMovieList():
     data = [
-        ('Alice in Wonderland', 2010, 3.4, 'Alice in Wonderland.jpg'),
-        ('Back to the Future', 1985, 4.5, 'Back to the Future.jpg'),
-        ('Casper', 1995, 3.3, 'Casper.jpg'),
-        ('Despicable Me', 2010, 4.0, 'Despicable Me.jpg'),
-        ('E.T.', 1982, 3.5, 'ET.jpg'),
-        ('Fast and Furious', 2009, 3.7, 'Fast and Furious.jpg'),
-        ('Ghostbusters', 1984, 4.2, 'Ghostbusters.jpg'),
-        ('Hacksaw Ridge', 2016, 4.3, 'Hacksaw Ridge.jpg'),
-        ('Indiana Jones', 2008, 3.3, 'Indiana Jones.jpg'),
-        ('Jumanji', 1995, 3.5, 'Jumanji.jpg'),
-        ('Kill Bill', 2003, 3.4, 'Kill Bill.jpg'),
-        ('Little Nicky', 2000, 3.3, 'Little Nicky.jpg'),
-        ('Matrix', 1999, 3.6, 'Matrix.jpg'),
-        ('Nacho Libre', 2006, 3.3, 'Nacho Libre.jpg'),
-        ('O brother, Where Art Thou', 2000, 4.1, 'OBrotherWhereArtThou.jpg'),
-        ('Priest', 2011, 3.2, 'Priest.jpg'),
-        ('Quarantine', 2008, 3.0, 'Quarantine.jpg'),
-        ('R.I.P.D.', 2013, 3.0, 'RIPD.jpg'),
-        ('Scary Movie', 2000, 3.0, 'Scary Movie.jpg'),
-        ('Toy Story', 1995, 3.8, 'Toy Story.jpg'),
-        ('UP.', 2009, 4.3, 'UP.jpg'),
-        ('V For Vendetta', 2005, 4.3, 'V For Vendetta.jpg'),
-        ('WALL-E', 2008, 4.3, 'WALL-E.jpg'),
-        ('X-MEN', 2000, 3.9, 'X-MEN.jpg'),
-        ('You''ve Got Mail', 1998, 3.8, 'Youve Got Mail.jpg'),
-        ('Zoolander', 2001, 4.0, 'Zoolander.jpg'),
+        {'title': 'Alice in Wonderland', 'year': 2010, 'rating': 3.4, 'image': 'Alice in Wonderland.jpg'},
+        {'title': 'Back to the Future', 'year': 1985, 'rating': 4.5, 'image': 'Back to the Future.jpg'},
+        {'title': 'Casper', 'year': 1995, 'rating': 3.3, 'image': 'Casper.jpg'},
+        {'title': 'Despicable Me', 'year': 2010, 'rating': 4.0, 'image': 'Despicable Me.jpg'},
+        {'title': 'E.T.', 'year': 1982, 'rating': 3.5, 'image': 'ET.jpg'},
+        {'title': 'Fast and Furious', 'year': 2009, 'rating': 3.7, 'image': 'Fast and Furious.jpg'},
+        {'title': 'Ghostbusters', 'year': 1984, 'rating': 4.2, 'image': 'Ghostbusters.jpg'},
+        {'title': 'Hacksaw Ridge', 'year': 2016, 'rating': 4.3, 'image': 'Hacksaw Ridge.jpg'},
+        {'title': 'Indiana Jones', 'year': 2008, 'rating': 3.3, 'image': 'Indiana Jones.jpg'},
+        {'title': 'Jumanji', 'year': 1995, 'rating': 3.5, 'image': 'Jumanji.jpg'},
+        {'title': 'Kill Bill', 'year': 2003, 'rating': 3.4, 'image': 'Kill Bill.jpg'},
+        {'title': 'Little Nicky', 'year': 2000, 'rating': 3.3, 'image': 'Little Nicky.jpg'},
+        {'title': 'Matrix', 'year': 1999, 'rating': 3.6, 'image': 'Matrix.jpg'},
+        {'title': 'Nacho Libre', 'year': 2006, 'rating': 3.3, 'image': 'Nacho Libre.jpg'},
+        {'title': 'O brother, Where Art Thou', 'year': 2000, 'rating': 4.1, 'image': 'OBrotherWhereArtThou.jpg'},
+        {'title': 'Priest', 'year': 2011, 'rating': 3.2, 'image': 'Priest.jpg'},
+        {'title': 'Quarantine', 'year': 2008, 'rating': 3.0, 'image': 'Quarantine.jpg'},
+        {'title': 'R.I.P.D.', 'year': 2013, 'rating': 3.0, 'image': 'RIPD.jpg'},
+        {'title': 'Scary Movie', 'year': 2000, 'rating': 3.0, 'image': 'Scary Movie.jpg'},
+        {'title': 'Toy Story', 'year': 1995, 'rating': 3.8, 'image': 'Toy Story.jpg'},
+        {'title': 'UP.', 'year': 2009, 'rating': 4.3, 'image': 'UP.jpg'},
+        {'title': 'V For Vendetta', 'year': 2005, 'rating': 4.3, 'image': 'V For Vendetta.jpg'},
+        {'title': 'WALL-E', 'year': 2008, 'rating': 4.3, 'image': 'WALL-E.jpg'},
+        {'title': 'X-MEN', 'year': 2000, 'rating': 3.9, 'image': 'X-MEN.jpg'},
+        {'title': 'You''ve Got Mail', 'year': 1998, 'rating': 3.8, 'image': 'Youve Got Mail.jpg'},
+        {'title': 'Zoolander', 'year': 2001, 'rating': 4.0, 'image': 'Zoolander.jpg'},
     ]
     return data
-
-
-def create_sqlite_database():
-    conn = sqlite3.connect("movies.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE movie(title, year, rating, file_name)")
-    data = [
-        ('Alice in Wonderland', 2010, 3.4, 'Alice in Wonderland.jpg'),
-        ('Back to the Future', 1985, 4.5, 'Back to the Future.jpg'),
-        ('Casper', 1995, 3.3, 'Casper.jpg'),
-        ('Despicable Me', 2010, 4.0, 'Despicable Me.jpg'),
-        ('E.T.', 1982, 3.5, 'ET.jpg'),
-        ('Fast and Furious', 2009, 3.7, 'Fast and Furious.jpg'),
-        ('Ghostbusters', 1984, 4.2, 'Ghostbusters.jpg'),
-        ('Hacksaw Ridge', 2016, 4.3, 'Hacksaw Ridge.jpg'),
-        ('Indiana Jones', 2008, 3.3, 'Indiana Jones.jpg'),
-        ('Jumanji', 1995, 3.5, 'Jumanji.jpg'),
-        ('Kill Bill', 2003, 3.4, 'Kill Bill.jpg'),
-        ('Little Nicky', 2000, 3.3, 'Little Nicky.jpg'),
-        ('Matrix', 1999, 3.6, 'Matrix.jpg'),
-        ('Nacho Libre', 2006, 3.3, 'Nacho Libre.jpg'),
-        ('O brother, Where Art Thou', 2000, 4.1, 'OBrotherWhereArtThou.jpg'),
-        ('Priest', 2011, 3.2, 'Priest.jpg'),
-        ('Quarantine', 2008, 3.0, 'Quarantine.jpg'),
-        ('R.I.P.D.', 2013, 3.0, 'RIPD.jpg'),
-        ('Scary Movie', 2000, 3.0, 'Scary Movie.jpg'),
-        ('Toy Story', 1995, 3.8, 'Toy Story.jpg'),
-        ('UP.', 2009, 4.3, 'UP.jpg'),
-        ('V For Vendetta', 2005, 4.3, 'V For Vendetta.jpg'),
-        ('WALL-E', 2008, 4.3, 'WALL-E.jpg'),
-        ('X-MEN', 2000, 3.9, 'X-MEN.jpg'),
-        ('You''ve Got Mail', 1998, 3.8, 'Youve Got Mail.jpg'),
-        ('Zoolander', 2001, 4.0, 'Zoolander.jpg'),
-    ]
-
-    cur.executemany("INSERT INTO movie VALUES(?, ?, ?, ?)", data)
-    conn.commit()
 
 
 def sqlite_insert(data: List[dict]):
     con_sqlite = sqlite3.connect("movies.db")
     cur = con_sqlite.cursor()
 
-    sql = "INSERT INTO resource VALUES(?,?,?,?,?,?)"
-    cur.executemany(sql, [list(i.values()) for i in data])
-    con_sqlite.commit()
+    try:
+        cur.execute("SELECT * FROM movie")  # check if the table already exists
+
+    except sqlite3.OperationalError:
+        print("Creating table: movie")
+        cur.execute("CREATE TABLE movie(title, year, rating, image)")
+        sql = "INSERT INTO movie VALUES(?,?,?,?)"
+        cur.executemany(sql, [list(i.values()) for i in data])
+
+    finally:
+        con_sqlite.commit()
+        con_sqlite.close()
 
 
 def writeHTML(name, year):
+
+    letter_from_name = getLetterFromName(name)
+    movie_1 = getMovieWithLetterInTitle(letter_from_name)
+    movie_2 = GetMovieBasedOnBirthYear(year)
+
     file = 'movies.html'
     with open(file, 'w') as f:
         f.write('\n')
@@ -144,11 +136,21 @@ def writeHTML(name, year):
         f.write('</head>\n')
         f.write('<body>\n')
         f.write('    <center>\n')
-        f.write(f'        <h1>Hi {name}. Here are some movie recommendations ',
-                'for you!</h1>\n')
+        f.write(f'        <h1>Hi {name}.</h1>\n')
+        f.write('        <h1>Here are some movie recommendations for you!</h1>\n')
         f.write('    </center>\n')
+        f.write('\n')
         f.write('    <hr />\n')
-        f.write(f'    {year}\n')
+        f.write('    <h2>Here is a movie that begins with a letter\n')
+        f.write(f'        from your name: {letter_from_name}</h2>\n')
+        f.write(f'    <img src="Project_Zakkary_Cook/images/{movie_1}"\n')
+        f.write('    width="250" style="display: block; margin: 30px auto;">\n')
+        f.write('\n')
+        f.write('    <hr />\n')
+        f.write(f'    <h2>Here is a movie from your birth year: {year}</h2>\n')
+        f.write(f'    <img src="Project_Zakkary_Cook/images/{movie_2}"\n')
+        f.write('    width="250" style="display: block; margin: 30px auto;">\n')
+        f.write('\n')
         f.write('    <hr />\n')
         f.write('</body>\n')
         f.write('</html>\n')
